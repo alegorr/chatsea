c8525a77341388eb3d3301882c04ecb69ac41eaa
eguruko <egor.richman@gmail.com>
2018-11-22 02:56:37 +0700

    Gather all macrodefs

---
+++ src/macrodefines.h
@@ -0,0 +1,26 @@
+#ifndef MACRODEFINES_H
+#define MACRODEFINES_H
+
+/*
+ * Port range macro
+ */
+#define MIN_PORT_NUMBER 0
+#define MAX_PORT_NUMBER 65535
+
+/*
+ * Client/Server communication defaults
+ */
+#define CONNECTION_PORT 8888
+#define MESSAGING_PORT 8889
+#define TIMEOUT 250
+
+/*
+ * Something more special
+ */
+#define CLASS_WITH_NAME                                                        \
+  string getClassName() {                                                      \
+    string name = typeid(*this).name();                                        \
+    return name.substr(2, name.size() - 2);                                    \
+  }
+
+#endif // MACRODEFINES_H
